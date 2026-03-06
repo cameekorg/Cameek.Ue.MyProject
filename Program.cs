ba42f816c40f291e01f103a44dfcb91d343463c2
jarosm3 <jarosm3@gmail.com>
2026-03-06 09:02:32 +0100

    Add PWA support with service worker and manifest file
    - Created a web manifest file for the application to define its properties and icons.
    - Implemented a service worker for offline support, including caching strategies for assets.
    - Added a published service worker script to handle installation, activation, and fetch events.
    - Configured static web app settings with caching headers and navigation fallback.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Cameek.Ue.MyProject;
+using Cameek.Primitivo.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
+
+builder.Services.AddUeServicesExtensions();
 
 await builder.Build().RunAsync();
